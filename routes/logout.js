4c2131e567a75b82cafb29de7891b779a683fc31
Prakash Padhi <sriprakashchpadhis@gmail.com>
2024-02-11 00:52:48 +0530

    Method changed to get

--- routes/logout.js
+++ routes/logout.js
@@ -7,7 +7,7 @@ const { cookieConfig } = require("../config/app-config");
  * @route GET api/logout
  * @desc Clear Cookie And Logout the user
  */
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         res.clearCookie("authcookie").status(200).json({ success: true, isAuthenticated: false, message: "Logout Successful." });
     } catch (error) {
